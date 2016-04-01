7ceb6902947a94fcc91cfe5445a71ba2ea0ae00b
Andrew Dorokhin <andrewnorilsk@gmail.com>
2016-04-01 22:49:25 +0700

    some fixes

--- db.py
+++ db.py
@@ -1,23 +1,13 @@
 import sqlite3
-from datetime import datetime
 import os.path
 import logging
-import random
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     filename='system.log')
 
-
-def local_time():
-    # Текущее время и дата
-    time = datetime.now().replace(microsecond=0)
-    return time
-
-
 #############################################################################
 db_file_name = "config.db"
-date_time = local_time()
 
 
 #############################################################################
@@ -40,7 +30,7 @@ def db_create(db_file_name_local):
     logging.debug('db file created')
 
 
-def db_write(db_file_name_local, mail, password, reg_date, userpic, is_admin, username):
+def db_write(mail, password, reg_date, userpic, is_admin, username, db_file_name_local=db_file_name):
     # База с данными пользователей
     db_connection = sqlite3.connect(db_file_name_local)
     db_conn_cursor = db_connection.cursor()
@@ -48,11 +38,6 @@ def db_write(db_file_name_local, mail, password, reg_date, userpic, is_admin, us
                   VALUES(?,?,?,?,?,?)''', (mail, password, reg_date, userpic, is_admin, username))
     # Save (commit) the changes
     db_connection.commit()
-    for row in db_conn_cursor.execute('SELECT * FROM users ORDER BY id'):
-        print(row)
-
-    # We can also close the connection if we are done with it.
-    # Just be sure any changes have been committed or they will be lost.
     db_connection.close()
 
 
@@ -60,16 +45,14 @@ def db_read(db_file_name_local, limit, last_value):
     # База с данными пользователей
     db_connection = sqlite3.connect(db_file_name_local)
     db_conn_cursor = db_connection.cursor()
-    data = db_conn_cursor.fetchall()
-    for row in db_conn_cursor.execute('SELECT * '
-                                      'FROM users '
-                                      'WHERE id > {0} '
-                                      'ORDER BY id '
-                                      'LIMIT {1}'.format(last_value, limit)):
-        print(row)
 
-    # We can also close the connection if we are done with it.
-    # Just be sure any changes have been committed or they will be lost.
+    db_conn_cursor.execute('SELECT * '
+                           'FROM users '
+                           'WHERE id > {0} '
+                           'ORDER BY id '
+                           'LIMIT {1}'.format(last_value, limit))
+
+    data = db_conn_cursor.fetchall()
     db_connection.close()
     return data
 
@@ -86,6 +69,3 @@ LIMIT 100
 if not (os.path.isfile(db_file_name)):
     logging.debug('db file not exist')
     db_create(db_file_name)
-
-
-print(random.randint(0, 1000))
--- logic.py
+++ logic.py
@@ -1,9 +1,13 @@
 import logging
 from flask import url_for
 from flask import redirect
-from db import db_create
-from db import db_write
+from db import db_write, db_read
+import string
+import random
+from datetime import datetime
 
+
+db_file_name = "config.db"
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -23,3 +27,38 @@ def log_the_user_in(username):
 def handle_message(name, email, message):
     logging.debug(name + " " + email + " " + message)
     return True
+
+
+def gen_pass(size=12, chars=string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def gen_mail(size=3, chars=string.ascii_letters + string.digits):
+
+    return ''.join(random.choice(chars) for _ in range(size)) + '@' + \
+           ''.join(random.choice(chars) for _ in range(size)) + '.' + \
+           ''.join(random.choice(chars) for _ in range(size))
+
+
+def gen_name(size=5, first_sign=string.ascii_uppercase , chars=string.ascii_letters):
+    return ''.join(random.choice(first_sign)) + ''.join(random.choice(chars) for _ in range(size))
+
+
+def local_time():
+    # Текущее время и дата
+    time = datetime.now().replace(microsecond=0)
+    return time
+
+
+def fill_db(iteration=1000):
+    for hop in range(iteration):
+        db_write(gen_mail(), gen_pass(), local_time(), 'none', 'false', gen_name())
+
+# fill_db()
+
+
+def user_list():
+    return db_read(db_file_name, 100, 0)
+
+print(user_list())
+
